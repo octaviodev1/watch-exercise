64ccf56b567df020936a814729daeeeec4653f2d
Octavian Badea <octavio.badea@gmail.com>
2023-10-11 19:27:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HtmlClock theme="rainbow"></HtmlClock>
+</template>
+
+<script>
+import HtmlClock from './components/HtmlClock.vue';
+export default {
+  components: {
+    HtmlClock,
+  },
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@700&family=Orbitron&display=swap');
+
+body {
+  font-family: 'Orbitron', sans-serif;
+}
+</style>
---
+++ src/components/HtmlClock.vue
@@ -0,0 +1,118 @@
+<template>
+    <div :style="backgroundStyle" class="background-container">
+        <div class="container">
+            <div class="hour"> {{ hour }}</div>
+            <div class="dots"> : </div>
+            <div class="minutes">{{ minutes }}</div>
+            <div class="dots"> : </div>
+            <div class="seconds"> {{ seconds }}</div>
+            <div class="pm-am">
+                {{ AmPm }} </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HtmlClock",
+    props: {
+        theme: {
+            rainbow: String
+        }
+    },
+    data() {
+        return {
+            hour: "",
+            minutes: "",
+            seconds: "",
+            AmPm: ""
+        }
+    },
+    mounted() {
+        console.log(this.theme)
+        setInterval(() => {
+            this.updateTime()
+        }, 1000);
+    },
+    methods: {
+        updateTime() {
+            const date = new Date();
+            let hour = date.getHours();
+            let minutes = date.getMinutes();
+            let seconds = date.getSeconds();
+            this.hour = hour.toString().padStart(2, "0");
+            this.minutes = minutes.toString().padStart(2, "0");
+            this.seconds = seconds.toString().padStart(2, "0");
+            console.log(hour)
+            if (hour > 13) {
+                this.AmPm = "PM"
+            } else {
+                this.AmPm = "AM"
+            }
+        },
+    }, computed: {
+        backgroundStyle() {
+            let backgroundColor;
+            switch (this.theme) {
+                case "rainbow":
+                    backgroundColor = 'linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)';
+                    break;
+                case "blue":
+                    backgroundColor = 'blue';
+                    break;
+                case "green":
+                    backgroundColor = 'green';
+                    break;
+            }
+            return {
+                background: backgroundColor,
+            }
+        }
+    },
+}
+
+</script>
+
+<style scoped>
+@keyframes appearDisappear {
+
+    0%,
+    100% {
+        opacity: 0;
+    }
+
+    50% {
+        opacity: 1;
+    }
+}
+
+.container {
+    width: 200px;
+    font-size: 30px;
+    background-color: black;
+    color: red;
+    padding: 15px;
+    border-radius: 7px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.background-container {
+    background-color: gray;
+    width: fit-content;
+    padding: 15px;
+    margin: 0 auto;
+}
+
+.pm-am {
+    margin-left: 10px;
+    font-size: 10px;
+    height: fit-content;
+}
+
+.dots {
+    animation: appearDisappear 1s ease infinite;
+}
+</style>
